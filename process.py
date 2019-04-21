a77ac00cc6c6856614e4feab1d782921fc1ce06d
vagrant <vagrant@vagrant.vm>
2019-04-21 06:10:47 +0000

    commenting code

--- process.py
+++ process.py
@@ -1,11 +1,15 @@
-clog_file = open("um-server-01.txt")
-
+log_file = open("um-server-01.txt")
+#opens text file
 
 def sales_reports(log_file):
     for line in log_file:
+        #loops through each line in file
         line = line.rstrip()
+        #removes leading whitespace
         day = line[0:3]
+        #keeps the first three index
         if day == "Mon":
+        #if day equals to Monday it prints
             print(line)
 
 
